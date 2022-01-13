ab9517951d842b653c6aff39cc987e282ad60ca1
Nikolay Osaulenko <northicewind@gmail.com>
2022-01-13 14:48:58 +0300

    Updated helper comment

--- lua/local_plugins.lua
+++ lua/local_plugins.lua
@@ -1,5 +1,6 @@
+-- To prevent git from tracking this file
+-- execute git update-index --assume-unchanged lua/local_plugins.lua
 return function(use)
 	-- Register local plugins here
 	-- Those are not to be installed at every computer
-	-- This file should not be tracked by git
 end
