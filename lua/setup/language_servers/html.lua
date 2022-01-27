35964b511a29e26fb58f2adee455f1e52654eb34
Nikolay Osaulenko <northicewind@gmail.com>
2022-01-27 21:47:29 +0300

    Disabled html language server formatting

--- lua/setup/language_servers/html.lua
+++ lua/setup/language_servers/html.lua
@@ -1,6 +1,11 @@
-return function(capabilites, on_attach)
+return function(capabilities, on_attach)
 	require("lspconfig").html.setup({
-		capabilities = capabilites,
-		on_attach = on_attach,
+		capabilities = capabilities,
+		on_attach = function(client)
+			-- Disable formatting. I'm using prettier for this
+			client.resolved_capabilities.document_formatting = false
+			client.resolved_capabilities.document_range_formatting = false
+			return on_attach(client)
+		end,
 	})
 end
