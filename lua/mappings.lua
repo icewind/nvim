85f702209a1ee9397feecb56bab80c83b1b493ab
Nikolay Osaulenko <northicewind@gmail.com>
2022-01-29 16:27:07 +0300

    Added more bufferline mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -55,7 +55,16 @@ map("n", "<leader>cd", '<cmd>lua require("telescope.builtin").diagnostics{ bufnr
 map("n", "<leader>cr", '<cmd>lua require("telescope.builtin").lsp_references()<cr>')
 
 -- Bufferline
-map("n", "<leader>b", ":BufferLinePick<cr>")
+map("n", "<leader>b", ":BufferLinePick<cr>", { noremap = true, silent = true })
+map("n", "<leader>1", "<Cmd>BufferLineGoToBuffer 1<CR>", { noremap = true, silent = true })
+map("n", "<leader>2", "<Cmd>BufferLineGoToBuffer 2<CR>", { noremap = true, silent = true })
+map("n", "<leader>3", "<Cmd>BufferLineGoToBuffer 3<CR>", { noremap = true, silent = true })
+map("n", "<leader>4", "<Cmd>BufferLineGoToBuffer 4<CR>", { noremap = true, silent = true })
+map("n", "<leader>5", "<Cmd>BufferLineGoToBuffer 5<CR>", { noremap = true, silent = true })
+map("n", "<leader>6", "<Cmd>BufferLineGoToBuffer 6<CR>", { noremap = true, silent = true })
+map("n", "<leader>7", "<Cmd>BufferLineGoToBuffer 7<CR>", { noremap = true, silent = true })
+map("n", "<leader>8", "<Cmd>BufferLineGoToBuffer 8<CR>", { noremap = true, silent = true })
+map("n", "<leader>9", "<Cmd>BufferLineGoToBuffer 9<CR>", { noremap = true, silent = true })
 
 -- Display diagnostics messages
 vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>", { silent = true, noremap = true })
