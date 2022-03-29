1b1bb70d593e52fd52b4cff24b82ae12332373da
Nikolay Osaulenko <northicewind@gmail.com>
2022-03-29 20:46:19 +0300

    Changed the way luasnip loads custom snippets

--- lua/setup/luasnip.lua
+++ lua/setup/luasnip.lua
@@ -2,7 +2,7 @@ local ls = require("luasnip")
 local files_in_folder = require("utils").files_in_folder
 
 for _, filetype in ipairs(files_in_folder({ "setup", "snippets" })) do
-	ls.snippets[filetype] = require(string.format("setup.snippets.%s", filetype)) or {}
+	ls.add_snippets(filetype, require(string.format("setup.snippets.%s", filetype)) or {})
 end
 
 require("luasnip.loaders.from_vscode").load()
