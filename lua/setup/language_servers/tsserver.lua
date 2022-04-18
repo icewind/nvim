b2fc845db4c4acba346eb7e9b82c7100936a051d
Nikolay Osaulenko <northicewind@gmail.com>
2022-04-18 22:35:21 +0300

    fixed an error in tsserver config

--- lua/setup/language_servers/tsserver.lua
+++ lua/setup/language_servers/tsserver.lua
@@ -1,11 +1,11 @@
 return function(capabilites, on_attach)
 	require("lspconfig").tsserver.setup({
 		capabilites = capabilites,
-		on_attach = function(client)
+		on_attach = function(client, bufnr)
 			-- Disable formatting. I'm using prettier for this
 			client.resolved_capabilities.document_formatting = false
 			client.resolved_capabilities.document_range_formatting = false
-			return on_attach(client)
+			return on_attach(client, bufnr)
 		end,
 	})
 end
