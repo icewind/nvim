ed26df02201e328b6aab8231e93ea9f51d3c2773
Nikolay Osaulenko <northicewind@gmail.com>
2022-08-25 12:47:22 +0300

    Added sqlfluff dialect

--- lua/setup/nullls.lua
+++ lua/setup/nullls.lua
@@ -3,10 +3,14 @@ null_ls.setup({
 	sources = {
 		-- Formatting
 		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.sqlfluff,
+		null_ls.builtins.formatting.sqlfluff.with({
+			extra_args = { "--dialect", "postgres" },
+		}),
 
 		-- Diagnostics
 		null_ls.builtins.diagnostics.eslint,
-		null_ls.builtins.diagnostics.sqlfluff,
+		null_ls.builtins.diagnostics.sqlfluff.with({
+			extra_args = { "--dialect", "postgres" },
+		}),
 	},
 })
