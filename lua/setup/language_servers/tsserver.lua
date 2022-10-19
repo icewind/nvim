04b1dac01d2ac3a5e06c909667607b639c7e30d0
Nikolay Osaulenko <northicewind@gmail.com>
2022-10-19 23:12:44 +0400

    Fixed tsserver

--- lua/setup/language_servers/tsserver.lua
+++ lua/setup/language_servers/tsserver.lua
@@ -4,8 +4,8 @@ return function(capabilites, on_attach)
 		capabilites = capabilites,
 		on_attach = function(client, bufnr)
 			-- Disable formatting. I'm using prettier for this
-			client.server_capabilities.document_formatting = false
-			client.server_capabilities.document_range_formatting = false
+			client.server_capabilities.documentFormattingProvider = false
+			client.server_capabilities.documentRangeFormattingProvider = false
 			return on_attach(client, bufnr)
 		end,
 	})
