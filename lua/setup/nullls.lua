96dfd4f23ae1c9c447bf45ec4ec88da9e893f129
Nikolay Osaulenko <northicewind@gmail.com>
2022-11-11 22:01:56 +0800

    Added cspell

--- lua/setup/nullls.lua
+++ lua/setup/nullls.lua
@@ -7,8 +7,12 @@ null_ls.setup({
 			extra_args = { "--dialect", "postgres" },
 		}),
 
+		-- Code actions
+		null_ls.builtins.code_actions.cspell,
+
 		-- Diagnostics
 		null_ls.builtins.diagnostics.eslint,
+		null_ls.builtins.diagnostics.cspell,
 		null_ls.builtins.diagnostics.sqlfluff.with({
 			extra_args = { "--dialect", "postgres" },
 		}),
