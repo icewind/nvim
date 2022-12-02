93ab6ff87910e3cd1c28bcf67f8a9e26bc9420da
Nikolay Osaulenko <northicewind@gmail.com>
2022-12-02 13:01:41 +0800

    Added gopls install instructions

--- lua/setup/language_servers/gopls.lua
+++ lua/setup/language_servers/gopls.lua
@@ -1,4 +1,5 @@
--- Gopls installed using standard golang installer
+-- Gopls installed using
+-- go install golang.org/x/tools/gopls@latest
 return function(_, on_attach)
 	require("lspconfig").gopls.setup({
 		on_attach = on_attach,
