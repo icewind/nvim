7ba693b7a2c1c5d72cf1011d15e9ca32ee5e622a
Nikolay Osaulenko <northicewind@gmail.com>
2022-12-02 12:59:21 +0800

    Fixed typo

--- lua/setup/language_servers/gopls.lua
+++ lua/setup/language_servers/gopls.lua
@@ -1,6 +1,6 @@
 -- Gopls installed using standard golang installer
 return function(_, on_attach)
-	require('lsp_config"').gopls.setup({
+	require("lspconfig").gopls.setup({
 		on_attach = on_attach,
 	})
 end
