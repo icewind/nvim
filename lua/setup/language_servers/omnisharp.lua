e7bbc295a1dc1bd108b287c51f3ded37cf53863b
Nikolay Osaulenko <northicewind@gmail.com>
2023-01-01 20:53:14 +0800

    Added gdscript and fixed typos

--- lua/setup/language_servers/omnisharp.lua
+++ lua/setup/language_servers/omnisharp.lua
@@ -2,22 +2,21 @@
 -- The language server itself is downloaded from the Github and stored in the filesystem
 -- manually downloaded from github
 local pid = vim.fn.getpid()
-local omnisharp_path = os.getenv("HOME") .. "/tools/omnisharp/run"
 
 return function(capabilities, on_attach)
-	require("lspconfig").omnisharp.setup({
-		capabilities = capabilities,
-		on_attach = function(_, bufnr)
-			vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-			-- Add lsp keymaps for this buffer
-			on_attach(_, bufnr)
-		end,
-		cmd = {
-			omnisharp_path,
-			"--languageserver",
-			"--hostPID",
-			tostring(pid),
-			"FormattingOptions:OrganizeImports=true",
-		},
-	})
+  require("lspconfig").omnisharp.setup({
+    capabilities = capabilities,
+    on_attach = function(_, bufnr)
+      vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+      -- Add lsp keymaps for this buffer
+      on_attach(_, bufnr)
+    end,
+    cmd = {
+      "omnisharp",
+      "--languageserver",
+      "--hostPID",
+      tostring(pid),
+      "FormattingOptions:OrganizeImports=true",
+    },
+  })
 end
--- lua/setup/language_servers/tsserver.lua
+++ lua/setup/language_servers/tsserver.lua
@@ -1,12 +1,12 @@
 -- npm i -g typescript-language-server
-return function(capabilites, on_attach)
-	require("lspconfig").tsserver.setup({
-		capabilites = capabilites,
-		on_attach = function(client, bufnr)
-			-- Disable formatting. I'm using prettier for this
-			client.server_capabilities.documentFormattingProvider = false
-			client.server_capabilities.documentRangeFormattingProvider = false
-			return on_attach(client, bufnr)
-		end,
-	})
+return function(capabilities, on_attach)
+  require("lspconfig").tsserver.setup({
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+      -- Disable formatting. I'm using prettier for this
+      client.server_capabilities.documentFormattingProvider = false
+      client.server_capabilities.documentRangeFormattingProvider = false
+      return on_attach(client, bufnr)
+    end,
+  })
 end
